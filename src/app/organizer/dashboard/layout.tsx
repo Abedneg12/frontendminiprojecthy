2ce71869de878104bd473f77ff66ffc55dd0faa3
Abedneg12 <yosua12.abednego@gmail.com>
2025-07-21 11:12:05 +0700

    feat: wrap DashboardLayout in StoreProvider for improved state management

--- src/app/organizer/dashboard/layout.tsx
+++ src/app/organizer/dashboard/layout.tsx
@@ -4,14 +4,16 @@ import Navbar from '@/components/Navbar';
 /*Dashboard layout*/
 import OrganizerSidebar from '@/components/organizer/OrganizerSidebar';
 import React from 'react';
+import StoreProvider from '@/lib/redux/storeProvider';
 
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen bg-gray-50">
-      <OrganizerSidebar />
-      
-      <main className="flex-1 p-6">{children}</main>
+      <StoreProvider>
+        <OrganizerSidebar />
+        <div className="flex-1 p-6">{children}</div>
+      </StoreProvider>
     </div>
   );
 }
