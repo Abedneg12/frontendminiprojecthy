ee1d3d87ff00a2f58f2138aca7ec428d0297e595
Abedneg12 <yosua12.abednego@gmail.com>
2025-07-21 11:06:32 +0700

    feat: refactor store setup to use makeStore function for improved clarity and flexibility

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,19 +1,21 @@
-// lib/redux/store.ts
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './slices/authSlice';
 import OrganizerProfileReducer from './slices/OrganizerProfileSlice';
 import customerProfileReducer from './slices/customerProfileSlice';
 import eventReducer from './slices/eventDataSlice';
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    OrganizerProfile: OrganizerProfileReducer,
-    customerProfile: customerProfileReducer,
-    events: eventReducer,
-  },
-});
 
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
-export type AppStore = typeof store;
+export function makeStore() {
+  return configureStore({
+    reducer: {
+        auth: authReducer,
+        OrganizerProfile: OrganizerProfileReducer,
+        customerProfile: customerProfileReducer,
+        events: eventReducer,
+    },
+  });
+}
+
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore["getState"]>;
+export type AppDispatch = AppStore["dispatch"];
--- src/lib/redux/storeProvider.tsx
+++ src/lib/redux/storeProvider.tsx
@@ -1,8 +1,18 @@
-// components/storeProvider.tsx
 "use client";
-import { Provider } from "react-redux";
-import { store } from "@/lib/redux/store";
 
-export default function StoreProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={store}>{children}</Provider>;
+import { useRef } from "react";
+import { Provider } from "react-redux";
+import { makeStore, AppStore } from "@/lib/redux/store";
+
+export default function StoreProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const storeRef = useRef<AppStore | null>(null);
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+
+  return <Provider store={storeRef.current}>{children}</Provider>;
 }
